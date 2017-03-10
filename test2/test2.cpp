14f277942d4a53fa093ff710002adac982e19bfa
EGG\bruce19950331 <bruce19950331@gmail.com>
2017-03-10 12:40:05 +0800

    完成

--- test2/test2.cpp
+++ test2/test2.cpp
@@ -4,7 +4,13 @@
 #include <iostream>
 using namespace System;
 bool Guess(int number) {
+	return 0;
 }
 int main(array<System::String ^> ^args)
 {
+	int Gss;
+	do {
+		std::cin >> Gss;
+	} while (!Guess(Gss));
+	return 0;
 }
